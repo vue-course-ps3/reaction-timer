bbd91a041adc2064215dad1a7c91d5ba2dca511d
maxim-peeters <maximpeeters38@gmail.com>
2025-01-06 21:02:51 +0100

    lesson 37

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">play</button>
   <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
-  <p v-if="showResults">Reaction time: {{ score }} ms</p>
   <Results v-if="showResults" :score="score"/>
 </template>
 
@@ -11,7 +10,7 @@ import Block from './components/Block.vue';
 import Results from './components/Results.vue';
 export default {
   name: 'App',
-  components: { Block },
+  components: { Block, Results },
   data(){
     return{
       isPlaying: false,
@@ -43,5 +42,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button{
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
